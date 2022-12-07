aa5f1d29f0b025034f5bb0ebe22e9dd1a98ec52c
Egor <112934317+Forehm@users.noreply.github.com>
2022-12-07 13:00:23 +0300

    Add files via upload

---
+++ SimpleTimer.h
@@ -0,0 +1,37 @@
+
+
+#include <string>
+#include <iostream>
+#include <chrono>
+
+
+
+using namespace std;
+
+
+
+class SimpleTimer
+{
+public:
+
+	SimpleTimer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+
+
+	string get_execution_time()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		float result = duration.count();
+		string res = to_string(result);
+		return res;
+	}
+
+private:
+	std::chrono::time_point<std::chrono::steady_clock> start;
+	std::chrono::time_point<std::chrono::steady_clock> end;
+	std::chrono::duration<float> duration;
+};
+
